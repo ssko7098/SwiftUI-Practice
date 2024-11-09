1ff450403c16ff114415fb250ae561fbf56ed255
Sebastian Skontos <ssko7098@uni.sydney.edu.au>
2024-11-09 16:06:54 +1100

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Sebastian Skontos on 9/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
